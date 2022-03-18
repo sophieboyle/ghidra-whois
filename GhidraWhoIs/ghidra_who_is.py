3cf8c1ac034a4a44a58b19ea9ec87df5d9af9213
Sophie Boyle <sophieboyle4028@gmail.com>
2022-03-18 18:24:54 +0000

    Completed simplistic script

--- GhidraWhoIs/ghidra_who_is.py
+++ GhidraWhoIs/ghidra_who_is.py
@@ -1,7 +1,33 @@
+# Checks for url strings and runs a whois query
 #@author Sophie Boyle https://github.com/sophieboyle
 #@category Strings
 #@keybinding
-#@menupath Window.OS_Command
+#@menupath Window.Who_Is
 #@toolbar
 
+from ghidra.program.util import DefinedDataIterator
+import subprocess
 
+try:
+    from urllib.parse import urlparse
+except ImportError:
+    from urlparse import urlparse
+
+def get_domains(strings):
+    domains = []
+    for s in strings:
+        url_str = urlparse(s)
+        if url_str.hostname:
+            domains.append(url_str.hostname)
+    return domains
+
+def main():
+    strings = [s.getDefaultValueRepresentation()[1:-1] \
+               for s in DefinedDataIterator.definedStrings(currentProgram)]
+    domains = get_domains(strings)
+    for domain in domains:
+        print('-'*100)
+        print(subprocess.check_output(["whois", ".".join(domain.split(".")[1:])]))
+    
+if __name__ == "__main__":
+    main()
